714721c42a4628befd7cbc8bea4889c3b49d6ca4
多田逸作 <tadaissa@tadaitsusakunoMac-mini.local>
2022-12-28 19:49:33 +0900

    issa

--- main.py
+++ main.py
@@ -5,21 +5,27 @@ print("じゃんけんゲーム　スタート！")
 
 time.sleep(1)
 
-janken_list = ["グー","チョキ","パー"]
+janken_list = ["グー", "チョキ", "パー"]
 print("グー=0, チョキ=1, パー=2")
 u = int(input("0.1.2の中から選ぼう！"))
-print(f"君は{janken_list}を選んだよ")
+print(f"君は{janken_list[u]}を選んだよ")
+
+time.sleep(1)
 
 c = random.randrange(0,2)
-print(f"コンピューターは{c}を選んだよ")
+print(f"コンピューターは{janken_list[c]}を選んだよ")
 
-i = (u-c) % 3
+time.sleep(1)
 
-if i == 1:
-      print("あなたの負け")
+reselts=[0,1,2]
 
-elif i ==2:
+reselt = (u-c) % 3
+
+if reselt == 2:
       print("あなたの勝ち！")
+
+elif reselt == 1:
+      print("あなたの負け。\nもう一1戦!")
 
 else:
       print("あいこ\nもう1戦！")
