d1e43e4f7e42d8daa14a52e9da853050bd675ec1
多田逸作 <tadaissa@tadaitsusakunoMac-mini.local>
2022-12-28 15:17:44 +0900

    issa

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("多田逸作")
+import random
+
+u=(1,2,3)
+print('''じゃんけんゲーム　スタート！"
+
+グー=0, チョキ=1, パー=2''')
+u = int(input("0.1.2の中から選ぼう！"))
+print(f"君は{u}を選んだよ")
+
+for n in range(1):
+      c = random.randrange(0,2)
+      print(f"コンピューターは{c}を選んだよ")
+
+i = (u-c) % 3
+
+if i == 1:
+      print("あなたの負け")
+
+elif i ==2:
+      print("あなたの勝ち！")
+
+else:
+      print("あなたの負け")
