4131dea57f3616ae95ed8150641365db2cb0a5ae
issatada <tadaissa@tadaitsusakunoMac-mini.local>
2022-12-29 18:08:53 +0900

    修正版1

--- main.py
+++ main.py
@@ -1,30 +1,35 @@
 import random
 import time
 
+waittime=1
+janken_list=["グー", "チョキ", "パー"]
+
 print("じゃんけんゲーム　スタート！")
 
-time.sleep(1)
+time.sleep(waittime)
 
-janken_list = ["グー", "チョキ", "パー"]
 print("グー=0, チョキ=1, パー=2")
-u = int(input("0.1.2の中から選ぼう！"))
-print(f"君は{janken_list[u]}を選んだよ")
+userhand= int(input("0.1.2の中から選ぼう！"))
+print(f"君は{janken_list[userhand]}を選んだよ")
 
-time.sleep(1)
+time.sleep(waittime)
 
-c = random.randrange(0,2)
-print(f"コンピューターは{janken_list[c]}を選んだよ")
+computerhand = random.randrange(0, 2)
+print(f"コンピューターは{janken_list[computerhand]}を選んだよ")
 
-time.sleep(1)
+time.sleep(waittime)
 
-reselts=[0,1,2]
+reselts=(0, 1, 2)
+win=str(2)
+rose=str(1)
 
-reselt = (u-c) % 3
+reselt = (userhand-computerhand) % 3
 
-if reselt == 2:
+
+if reselt == win:
       print("あなたの勝ち！")
 
-elif reselt == 1:
+elif reselt == rose:
       print("あなたの負け。\nもう一1戦!")
 
 else:
